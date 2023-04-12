80c534577fb6a9b096d9c28fa26fb22943554640
Yan Fung <fungmingyan@hotmail.com>
2023-04-12 22:44:51 +0800

    Added test to check if the app being rendered correctly with test passing

---
+++ src/tests/App.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "../components/App";
+
+it("renders app correctly", () => {
+  const { asFragment } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
