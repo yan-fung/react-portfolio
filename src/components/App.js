8df84a83abe814d2be7debf3f24035e2d8e2f61e
Yan Fung <fungmingyan@hotmail.com>
2023-04-13 13:03:36 +0800

    Created selected page state variable and passed down to Navbar component as props and displays the Navbar. Added isAboveMediumScreens variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const App = () => {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumSrceens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 };
