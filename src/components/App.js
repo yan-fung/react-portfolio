8e3051fa6246b3c5f27307702ffc7adf4ba20e2e
Yan Fung <fungmingyan@hotmail.com>
2023-04-14 16:01:37 +0800

    Created handleScroll function and uses useEffect to check whether it's top of page for the first render. Displays DotGroup element when it is above medium screens

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import DotGroup from "./DotGroup";
 import useMediaQuery from "../hooks/useMediaQuery";
 
 const App = () => {
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumSrceens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5-6 mx-auto md:h-full">
+        {isAboveMediumSrceens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 };
