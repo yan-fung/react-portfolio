9efc63ef39306f842ad22ac17682b9dda31b1b50
Yan Fung <fungmingyan@hotmail.com>
2023-04-16 12:22:32 +0800

    Added image section and main section with my intro and links for contact me and social media accounts

---
+++ src/components/Landing.js
@@ -0,0 +1,114 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+import useMediaQuery from "../hooks/useMediaQuery";
+import SocialMediaIcons from "./SocialMediaIcons";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center md:H-full gap-5 py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:rounded-t-[400px]
+          before:w-full before:max-w-[400px] md:before:max-w-[400px] before:h-full before:border-8 before:border-white before:z-[-1]"
+          >
+            <img
+              className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[250px] md:max-w-[450px]"
+              alt="profile"
+              src="../assets/profile-pic.png"
+            />
+          </div>
+        ) : (
+          <img
+            className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            alt="profile"
+            src="../assets/profile-pic.png"
+          />
+        )}
+      </div>
+
+      {/* MAIN SECTION */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32 md:mx-20">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.8 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl text-black font-playfair z-10 text-center md:text-start md:pl-12">
+            Yan{" "}
+            <span
+              className="xs:relative xs:text-black xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[120px] before:z-[-1]"
+            >
+              Fung
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-2xl text-center md:text-start md:pl-12 xs:px-5">
+            This is my portfolio. Software Engineering Bootcamp student with a
+            strong passion for technology. Pursuing a career in Software
+            Development after switching from the legal industry.
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.8 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-brown rounded-sm py-3 px-7 md:ml-12 font-semibold hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            <div className="bg-dark-pink font-semibold hover:text-white transition duration-500 w-full h-full flex items-center justify-center px-10">
+              Let&apos;s talk
+            </div>
+          </AnchorLink>
+        </motion.div>
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.8 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
