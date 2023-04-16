e3fdf840d589711846395394f783371e054a9fc0
Yan Fung <fungmingyan@hotmail.com>
2023-04-16 19:16:14 +0800

    Removed the useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Landing from "./Landing";
 import DotGroup from "./DotGroup";
