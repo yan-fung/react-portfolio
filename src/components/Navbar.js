d5834a3a0f72bfbf20aab6e2076a0d33f51d35dc
Yan Fung <fungmingyan@hotmail.com>
2023-04-16 19:41:22 +0800

    Removed the Link component and uses AnchorLink component instead

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,12 @@ import React, { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Link = ({ page, selectedPage, setSelectedPage }) => {
-  const lowerCasePage = page.toLowerCase();
-  return (
-    <AnchorLink
-      className={`${selectedPage === lowerCasePage ? "text-white" : ""}
-      hover:text-yellow transition duration-500`}
-      onClick={() => setSelectedPage(lowerCasePage)}
-    >
-      {page}
-    </AnchorLink>
-  );
-};
-
-const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+const Navbar = ({ setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-  const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+    <nav className="bg-darker-pink z-40 w-full fixed top-0 py-6">
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold text-transparent bg-clip-text   bg-gradient-to-r from-rose-100 to-teal-100">
           YAN FUNG
@@ -31,26 +17,27 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
         {/* DESKTOP NAV */}
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-xl font-semibold">
-            <Link
-              page="Home"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Skills"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Projects"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Contact"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
+            <AnchorLink onClick={() => setSelectedPage("home")} href="#home">
+              Home
+            </AnchorLink>
+            <AnchorLink
+              onClick={() => setSelectedPage("skills")}
+              href="#skills"
+            >
+              Skills
+            </AnchorLink>
+            <AnchorLink
+              onClick={() => setSelectedPage("projects")}
+              href="#projects"
+            >
+              Projects
+            </AnchorLink>
+            <AnchorLink
+              onClick={() => setSelectedPage("contact")}
+              href="#contact"
+            >
+              Contact
+            </AnchorLink>
           </div>
         ) : (
           <button
@@ -64,7 +51,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
 
         {/* MOBILE MENU POPUP */}
         {!isAboveSmallScreens && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+          <div className="fixed right-0 bottom-0 h-full bg-white opacity-80 w-[300px]">
             {/* CLOSE ICON */}
             <div className="flex justify-end p-12">
               <button
@@ -76,27 +63,28 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
             </div>
 
             {/* MENU ITEMS */}
-            <div className="flex flex-col gap-10 ml-[33%] text-2x1 text-deep-blue">
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Skills"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Projects"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
+            <div className="flex flex-col gap-10 ml-[33%] text-2x1 text-brown">
+              <AnchorLink onClick={() => setSelectedPage("home")} href="#home">
+                Home
+              </AnchorLink>
+              <AnchorLink
+                onClick={() => setSelectedPage("skills")}
+                href="#skills"
+              >
+                Skills
+              </AnchorLink>
+              <AnchorLink
+                onClick={() => setSelectedPage("projects")}
+                href="#projects"
+              >
+                Projects
+              </AnchorLink>
+              <AnchorLink
+                onClick={() => setSelectedPage("contact")}
+                href="#contact"
+              >
+                Contact
+              </AnchorLink>
             </div>
           </div>
         )}
