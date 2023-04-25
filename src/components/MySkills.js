ebf3948b5424162d3e73d615f8095ab666b2390d
Yan Fung <fungmingyan@hotmail.com>
2023-04-25 08:50:06 +0800

    Added the icons for front-end, back-end and tools

--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -7,11 +7,11 @@ const MySkills = () => {
   const isAboveMediumSrceens = useMediaQuery("(min-width: 1060px)");
 
   return (
-    <section id="skills" className="pt-10 pb-24">
+    <section id="skills" className="pt-5 pb-24">
       {/* HEADER AND IMGAGE SECTION */}
-      <div className="md: flex md:justify-between md:gap-16 mt-32 pl-20 pr-20">
+      <div className="md:flex md:justify-between gap-16 mt-32">
         <motion.div
-          className="md:w-1/3"
+          className="md:w-2/3"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -25,15 +25,23 @@ const MySkills = () => {
             MY <span className="text-pink-700">SKILLS</span>
           </p>
           <LineGradient width="w-1/3" />
-          <p className="mt-10 rm-7">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo.
+          <p className="mt-10 rm-7 md:text-lg text-sm">
+            I received training to become a full stack developer from a bootcamp
+            course from Command Shift. Starting with the basic concepts of
+            programming, I progressed to building web applications using the
+            React framework with API servers and databases. Through this
+            experience, I gained a solid understanding of how front-end and
+            back-end technologies work together, as well as best practices such
+            as Test-Driven Development and Object-Oriented Programming.
           </p>
+          <div className="pt-5 md:text-lg text-sm">
+            Throughout the bootcamp, I completed several challenging coding
+            projects, including a web application that involved integrating
+            databases. Even after finishing the course, I continue to learn and
+            explore new technologies by building projects in my free time.
+          </div>
         </motion.div>
-        <div className="mt-16 md:mt-0">
+        <div className="mt-16 md:text-lg text-sm">
           {isAboveMediumSrceens ? (
             <div
               className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
@@ -52,7 +60,7 @@ const MySkills = () => {
       </div>
 
       {/* SKILLS */}
-      <div className="md:flex md:justify-between mt-16 gap-32 pl-20 pr-20">
+      <div className="md:flex md:justify-between mt-10 gap-32 pl-10 pr-10">
         {/* FRONT-END */}
         <motion.div
           className="md:w-1/3 mt-10"
@@ -72,15 +80,34 @@ const MySkills = () => {
                 FRONT-END
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-blue opacity-80 absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-4/5 h-32 bg-blue opacity-80 absolute right-0 top-0 z-[-1]" />
           </div>
-          <p className="mt-5">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo.
-          </p>
+          <div className="flex justify-center">
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/react-logo.png"
+                alt="react-logo"
+              />
+            </div>
+            <div className="p-3">
+              <img className="pt-5" src="./assets/js-icon.png" alt="js-logo" />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/html-icon.png"
+                alt="html-logo"
+              />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/css-icon.png"
+                alt="css-logo"
+              />
+            </div>
+          </div>
         </motion.div>
         {/* BACK-END */}
         <motion.div
@@ -101,17 +128,40 @@ const MySkills = () => {
                 BACK-END
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-rose-600 opacity-80 absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-4/5 h-32 bg-rose-600 opacity-80 absolute right-0 top-0 z-[-1]" />
           </div>
-          <p className="mt-5">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo.
-          </p>
+          <div className="flex justify-center">
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/node-icon.png"
+                alt="react-logo"
+              />
+            </div>
+            <div className="p-3 w-40 pt-6">
+              <img
+                className="pt-5"
+                src="./assets/express-icon.png"
+                alt="js-logo"
+              />
+            </div>
+            <div className="p-3 w-20">
+              <img
+                className="pt-5"
+                src="./assets/postman-icon.png"
+                alt="html-logo"
+              />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/postgresql-icon.png"
+                alt="css-logo"
+              />
+            </div>
+          </div>
         </motion.div>
-        {/* AGILE */}
+        {/* TOOLS */}
         <motion.div
           className="md:w-1/3 mt-10"
           initial="hidden"
@@ -126,19 +176,36 @@ const MySkills = () => {
           <div className="relative h-32">
             <div className="z-10">
               <p className="font-playfair font-semibold text-5xl">03</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">
-                AGILE PRACTICES
-              </p>
+              <p className="font-playfair font-semibold text-3xl mt-3">TOOLS</p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-yellow opacity-80 absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-4/5 h-32 bg-yellow opacity-80 absolute right-0 top-0 z-[-1]" />
           </div>
-          <p className="mt-5">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo.
-          </p>
+          <div className="flex justify-center">
+            <div className="pr-3 pt-2">
+              <img src="./assets/git.png" alt="git-icon" />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/docker-icon.png"
+                alt="docker-logo"
+              />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/vscode-icon.png"
+                alt="vscode-logo"
+              />
+            </div>
+            <div className="p-3">
+              <img
+                className="pt-5"
+                src="./assets/github.png"
+                alt="github-logo"
+              />
+            </div>
+          </div>
         </motion.div>
       </div>
     </section>
